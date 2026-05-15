61eafeaccea4deda10d3359a933a3dabf33265c4
andrey <defario777@mail.ru>
2026-05-15 17:20:46 +0500

    setup logger

--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 
 	"github.com/1DelFin1/testMicroservice/config"
+	"github.com/1DelFin1/testMicroservice/internal/lib/logger"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
+
+	log := logger.SetupLogger(cfg.App.Env)
+	log.Info("Logger initialized")
 }
